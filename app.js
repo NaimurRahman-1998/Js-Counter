c8276a0dc24464ba86c314367cfb1fb10882881a
NaimurRahman-1998 <nrturzo1998111@gmail.com>
2023-10-26 14:35:44 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+let count = 0;
+
+const value = document.querySelector('#value')
+const btns = document.querySelectorAll('.btn')
+
+// console.log(btns);
+
+btns.forEach((btn)=>{
+    console.log(btn)
+    btn.addEventListener('click' , (e)=>{
+        const styles = e.currentTarget.classList
+        console.log(styles);
+        if(styles.contains("decrease")){
+            count--
+        }else if (styles.contains("increase")){
+            count++
+        }else{ count = 0 } 
+
+        value.textContent = count
+    })
+    
+})
